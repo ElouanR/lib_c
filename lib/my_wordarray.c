56d2b9dde015191809a87fc667a5207bd82006f3
ElouanR <elouan.rigomont@epitech.eu>
2023-03-19 01:10:51 +0100

    [~] free wordarray + coding style

--- lib/my_wordarray.c
+++ lib/my_wordarray.c
@@ -6,7 +6,6 @@
 */
 
 #include "./lib.h"
-#include <stdlib.h>
 
 int get_words(char *str)
 {
@@ -22,27 +21,45 @@ int get_words(char *str)
     return (nbr);
 }
 
+char *modif_str_two(char *str)
+{
+    char *temp = malloc(sizeof(char) * (my_strlen(str) + 1));
+    int s = 0;
+    int e = 0;
+    int i = 0;
+
+    if (str == NULL || temp == NULL)
+        return (NULL);
+    for (;str[s] && (str[s] == ' ' || str[s] == '\n' || str[s] == '\t'); s++);
+    for (e = my_strlen(str) - 1 ; e > 0 && (str[e] == ' '
+    || str[e] == '\n' || str[e] == '\t') ; e--);
+    for (i = 0; s <= e; s++, i++)
+        temp[i] = str[s];
+    temp[i] = '\0';
+
+    return (temp);
+}
+
 char *modif_str(char *str)
 {
     char *temp = malloc(sizeof(char) * (my_strlen(str) + 1));
-    int s = 0, e = 0, i = 0, x = 0;
-    for (;str[s] && (str[s] == ' ' || str[s] == '\n' || str[s] == '\t') ; s++);
-    for (e = my_strlen(str) - 1 ; e > 0 && (str[e] == ' '
-    || str[e] == '\n' || str[e] == '\t') ; e--);
-    for (i = 0 ; s <= e ; s++, i++)
-        temp[i] = str[s];
-    temp[i] = '\0'; str = temp;
-    temp = malloc(sizeof(char) * (my_strlen(str) + 1));
-    i = 0;
-    for (int x = 0 ; str[x] ; x++) {
+    int i = 0;
+    int x = 0;
+
+    str = modif_str_two(str);
+    if (temp == NULL || str == NULL)
+        return (NULL);
+    for (int x = 0; str[x]; x++) {
         int z = 0;
         if (str[x] != ' ' && str[x] != '\n' && str[x] != '\t')
             temp[i++] = str[x];
         else
             temp[i++] = ' ';
-        for (; str[x] == ' ' || str[x] == '\n' || str[x] == '\t' ; x++, z++);
+        for (; str[x] == ' ' || str[x] == '\n' || str[x] == '\t'; x++, z++);
         (z != 0) ? x-- : 0;
-    } temp[i] = '\0';
+    }
+    temp[i] = '\0';
+    free(str);
     return (temp);
 }
 
@@ -52,6 +69,8 @@ char **my_wordarray(char *str)
     char *temp = modif_str(str);
     char **array = malloc(sizeof(char *) * (get_words(temp) + 1));
 
+    if (temp == NULL || array == NULL)
+        return (NULL);
     for (; x < get_words(temp) ; x++) {
         array[x] = malloc(sizeof(char) * (my_strlen(temp) + 1));
         int a = 0;
@@ -62,6 +81,6 @@ char **my_wordarray(char *str)
         t++;
     }
     array[x] = NULL;
-
+    free(temp);
     return (array);
 }
